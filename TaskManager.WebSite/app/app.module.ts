3ad5f5713c2b3ff512449f7776184d543e3cd2c1
joncoello <joncoello@gmail.com>
2016-12-20 20:53:41 +0000

    wire up ng2 in client

---
+++ TaskManager.WebSite/app/app.module.ts
@@ -0,0 +1,13 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { TaskListComponent }  from './app.taskListComponent';
+
+@NgModule({
+    imports: [
+        BrowserModule
+    ],
+    declarations: [TaskListComponent],
+    bootstrap: [TaskListComponent]
+})
+export class AppModule { }
---
+++ TaskManager.WebSite/app/app.taskListComponent.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>{{title}}</h1>'
+})
+export class TaskListComponent {
+    title: string;
+
+    constructor() {
+        this.title = "blah blah blah";
+    }
+}
