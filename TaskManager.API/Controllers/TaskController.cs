4fd78d349c5e5c07003a45892999a517a3ec4d4c
joncoello <joncoello@gmail.com>
2016-12-27 20:45:57 +0000

    add task endpoint and client list

---
+++ TaskManager.API/Controllers/TaskController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TaskManager.API.Models;
+
+namespace TaskManager.API.Controllers
+{
+    [RoutePrefix("api/task")]
+    public class TaskController : ApiController
+    {
+        [Route("")]
+        public async Task<object> Get()
+        {
+            return new List<TaskItem> {
+                new TaskItem {
+                    ID = Guid.NewGuid(),
+                    Name = "Task 1"
+                },
+                new TaskItem{
+                    ID = Guid.NewGuid(),
+                    Name = "Task 2"
+                }
+            };
+        }
+    }
+}
--- TaskManager.WebSite/app/app.taskListComponent.ts
+++ TaskManager.WebSite/app/app.taskListComponent.ts
@@ -6,20 +6,33 @@ import { Observable }     from 'rxjs/Observable';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1>'
+    template: `
+    <h1>{{title}}</h1>
+    <ul>
+       <li *ngFor="let item of data">
+          {{item.name}}
+       </li>
+    </ul>
+`
 })
 export class TaskListComponent {
     title: string;
-    data: Object;
+    data: Object[];
 
-    private heroesUrl = 'http://localhost:42992/api/hello';  // URL to web API
+    private helloUrl = 'http://localhost:42992/api/hello';  // URL to web API
+    private tasksUrl = 'http://localhost:42992/api/task';  // URL to web API
 
     constructor(private http: Http) {
         this.title = "loading...";
-
-        this.http.get(this.heroesUrl)
+        
+        this.http.get(this.helloUrl)
             .subscribe((res: Response) => {
                 this.title = res.json().message;
+            });
+
+        this.http.get(this.tasksUrl)
+            .subscribe((res: Response) => {
+                this.data = res.json();
             });
     }
 }
