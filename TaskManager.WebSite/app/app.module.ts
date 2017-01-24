406fed9e9bba3d3407359e7d08647825ab02e8dc
Jon Coello <joncoello@gmail.com>
2017-01-24 22:31:43 +0000

    add comments to entry files add basic form to task detail

--- TaskManager.WebSite/app/app.module.ts
+++ TaskManager.WebSite/app/app.module.ts
@@ -1,8 +1,11 @@
-﻿import { NgModule }      from '@angular/core';
+﻿// entry point module
+
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule }      from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 import {APP_BASE_HREF} from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/app.homeComponent';
@@ -10,6 +13,8 @@ import { TaskListComponent }  from './task/app.task-list.component';
 import { TaskComponent }  from './task/app.task.component';
 import { AboutComponent }  from './about/app.aboutcomponent';
 
+// need to move this to its own module
+// clarify lazy loading approach
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
@@ -22,7 +27,9 @@ const routes: Routes = [
     imports: [
         BrowserModule,
         HttpModule,
-        RouterModule.forRoot(routes)
+        RouterModule.forRoot(routes),
+        FormsModule,
+        ReactiveFormsModule
     ],
     declarations: [
         AppComponent,
--- TaskManager.WebSite/app/task/app.task.component.ts
+++ TaskManager.WebSite/app/task/app.task.component.ts
@@ -5,7 +5,20 @@ import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'task',
-    template: '<h2>{{pageTitle}}</h2>'
+    template: `
+        <h2>{{pageTitle}}</h2>
+        <form   #f="ngForm"
+                (ngSubmit)="onSubmit(f.value)">
+            <div>
+                <label for="skuInput">SKU</label>
+                <input  type="text"
+                        id="skuInput"
+                        placeholder="SKU"
+                        name="sku" ngModel>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    `
 })
 export class TaskComponent {
     pageTitle: string = 'Task';
@@ -14,6 +27,10 @@ export class TaskComponent {
         route.params.subscribe((params: {[key: string] : any}) => {
             this.pageTitle = params['id'];
         });
+    }
+
+    onSubmit(form: any): void {
+        console.log('you submitted value:', form);
     }
 
 }
