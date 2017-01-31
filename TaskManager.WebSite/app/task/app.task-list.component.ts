84de772cdce2e4d9db78259f4a8ffca3419c075b
Jon.Coello <Jon.Coello@WLKCND4071HPV>
2017-01-31 11:17:59 +0000

    support create tasks

--- TaskManager.WebSite/app/task/app.task-list.component.ts
+++ TaskManager.WebSite/app/task/app.task-list.component.ts
@@ -2,10 +2,22 @@
 
 import { Http, Response } from '@angular/http';
 
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 @Component({
     selector: 'tasks',
     template: `
     <h1>{{title}}</h1>
+    <form   [formGroup]="myForm"
+            (ngSubmit)="onSubmit(myForm.value)">
+
+        <input  type="text"
+                id="name"
+                [formControl]="myForm.controls['name']">
+
+        <button type="submit">Submit</button>
+
+    </form>
     <ul>
        <li *ngFor='let item of data'>
           <a [routerLink]="['task', item.id]">{{item.name}}</a>
@@ -17,13 +29,18 @@ import { Http, Response } from '@angular/http';
 export class TaskListComponent {
     title: string;
     data: Object[];
+    myForm: FormGroup;
 
     private baseUrl = 'http://jctaskmanagerapi.azurewebsites.net/';
     private helloUrl = this.baseUrl + '/api/hello';  // url to web API
     private tasksUrl = this.baseUrl + '/api/task';  // url to web API
 
-    constructor(private http: Http) {
+    constructor(private http: Http, fb: FormBuilder) {
         this.title = 'loading...';
+
+        this.myForm = fb.group({
+            'name': ['ABC123']
+        });
 
         this.http.get(this.helloUrl)
             .subscribe((res: Response) => {
@@ -35,4 +52,13 @@ export class TaskListComponent {
                 this.data = res.json();
             });
     }
+
+    onSubmit(form: any): void {
+        console.log('you submitted value:', form);
+        this.http.post(this.tasksUrl, form)
+            .subscribe((res: Response) => {
+                console.log(res.statusText);
+            });
+    }
+
 }
