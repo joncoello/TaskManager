bd16477fc62cd90fb02972fe176b3510ac98297a
Jon.Coello <Jon.Coello@WLKCND4071HPV>
2017-02-01 23:42:06 +0000

    increase height of body

--- TaskManager.WebSite/app/task/app.task.component.ts
+++ TaskManager.WebSite/app/task/app.task.component.ts
@@ -1,7 +1,8 @@
 ﻿/* tslint:disable:no-string-literal */
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Http, Response } from '@angular/http';
 
 @Component({
     selector: 'task',
@@ -15,40 +16,65 @@ import { FormBuilder, FormGroup } from '@angular/forms';
                 <label class="form-control">{{pageTitle}}</label>
             </div>
             <div class="form-group">
-                <label for="skuInput">Name</label>
+                <label for="nameInput">Name</label>
                 <input  class="form-control"
                         type="text"
-                        id="skuInput"
+                        id="nameInput"
                         placeholder="Name"
-                        [formControl]="myForm.controls['sku']">
+                        [formControl]="myForm.controls['name']">
             </div>
             <div class="form-group">       
-                <!--<textarea class="multi-line-text" placeholder="What's up?" required></textarea>-->
                 <label for="body">Body</label>
-                <textarea id="body" class="form-control" placeholder="Description" required></textarea>
+                <textarea   id="body" 
+                            style="height: 200px;"
+                            class="form-control" 
+                            placeholder="Description" 
+                            required
+                            [formControl]="myForm.controls['body']">
+                </textarea>
             </div>
             <button class="btn btn-primary" type="submit">Submit</button>
         </form>
     `
 })
-export class TaskComponent {
-    pageTitle: string = 'Task';
-    myForm: FormGroup;
+export class TaskComponent implements OnInit {
+    public pageTitle: string = 'Task';
+    public myForm: FormGroup;
+    public nameField: FormControl;
+    public bodyField: FormControl;
 
-    constructor(private route: ActivatedRoute, fb: FormBuilder) {
-        this.myForm = fb.group({
-            'sku': ['ABC123']
+    private baseUrl = 'http://jctaskmanagerapi.azurewebsites.net';
+    // private baseUrl = 'http://localhost:42992';
+    private tasksUrl = this.baseUrl + '/api/task';  // url to web API
+
+    constructor(private http: Http, private route: ActivatedRoute, private fb: FormBuilder) {
+        this.nameField = new FormControl('');
+        this.bodyField = new FormControl('');
+        this.myForm = this.fb.group({
+            'name': this.nameField,
+            'body': this.bodyField
         });
-
-        route.params.subscribe((params: { [key: string]: any }) => {
-            this.pageTitle = params['id'];
-        });
-
-        
-
     }
 
-    onSubmit(form: any): void {
+    public ngOnInit() {
+
+        this.route.params.subscribe((params: { [key: string]: any }) => {
+            var id = params['id'];
+
+            this.pageTitle = id;
+
+            this.http.get(this.tasksUrl + '/' + id)
+                .subscribe((res: Response) => {
+                    console.log(res);
+                    this.nameField.patchValue(res.json().task.name);
+                    this.bodyField.patchValue(res.json().task.body);
+                    console.log('updated');
+                });
+
+        });
+    }
+
+    public onSubmit(form: any): void {
         console.log('you submitted value:', form);
     }
 
