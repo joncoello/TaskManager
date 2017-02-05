84c6d265cc1c3aa945da2801cd28b7f117134d39
Jon Coello <joncoello@gmail.com>
2017-02-05 19:18:58 +0000

    update to caching fix

--- TaskManager.API/Infrastructure/NoCacheHeaderFilter.cs
+++ TaskManager.API/Infrastructure/NoCacheHeaderFilter.cs
@@ -8,6 +8,9 @@ namespace TaskManager.API.Infrastructure {
     public class NoCacheHeaderFilter : ActionFilterAttribute {
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext) {
             actionExecutedContext.Response.Headers.Add("Cache-Control", "no-store, must-revalidate, no-cache, max-age=0");
+            actionExecutedContext.Response.Headers.Add("Pragma", "no-cache");
+            actionExecutedContext.Response.Headers.Add("Expires", "Mon, 01 Jan 1990 00:00:00 GMT");
+            //Pragma:no-cache
         }
     }
 }
