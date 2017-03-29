6be0b9dcc8089703a7070b06e0d5d5fd8856cfd1
Jon Coello <joncoello@gmail.com>
2017-03-29 16:16:30 +0100

    add scaffolding for sql repositories

---
+++ TaskManager.SqlRepositories/TaskCategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.DomainModel.Entities;
+
+namespace TaskManager.SqlRepositories
+{
+    public class TaskCategoryRepository
+    {
+        public Task Create(TaskCategory newTaskCategory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TaskManager.SqlRepositoriesTests/TaskCategoryRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.DomainModel.Entities;
+using TaskManager.SqlRepositories;
+using Xunit;
+
+namespace TaskManager.SqlRepositoriesTests
+{
+    public class TaskCategoryRepositoryTests
+    {
+
+        [Fact]
+        public async Task TaskCategoryRepository_Create() {
+
+            var newTaskCategory = new TaskCategory();
+
+            var sut = new TaskCategoryRepository();
+
+            await sut.Create(newTaskCategory);
+
+        }
+
+    }
+}
