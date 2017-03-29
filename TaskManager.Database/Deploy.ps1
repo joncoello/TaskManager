a4cf96fabee59f0a27afa19c769e9f82117e6615
Jon Coello <joncoello@gmail.com>
2017-03-29 06:00:16 +0100

    add deploy.ps1 to database project

---
+++ TaskManager.Database/Deploy.ps1
@@ -0,0 +1,12 @@
+﻿$dbName = $OctopusParameters['DbName']
+$user = $OctopusParameters['DbUser']
+$pwd = $OctopusParameters['DbPassword']
+$server = $OctopusParameters['DbServerName']
+
+Write-Host DbName: $dbName
+Write-Host User: $user
+Write-Host Server: $server
+
+& 'C:\Program Files (x86)\Microsoft SQL Server\120\DAC\bin\SqlPackage.exe' /Action:Script /OutputPath:Upgrade.publish.sql /SourceFile:TaskManager.Database.dacpac /TargetServerName:$server /TargetDatabaseName:$dbName /p:IncludeCompositeObjects=true /TargetUser:$user /TargetPassword:$pwd
+
+& sqlcmd -S $server -U $user -P $pwd -i "Upgrade.publish.sql"
