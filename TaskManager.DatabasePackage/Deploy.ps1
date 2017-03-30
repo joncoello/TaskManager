567a8de91353ef450273f073b664f52bab4cbeec
Jon Coello <joncoello@gmail.com>
2017-03-30 06:23:35 +0100

    update deploy.ps1 to pick up current directory

--- TaskManager.DatabasePackage/Deploy.ps1
+++ TaskManager.DatabasePackage/Deploy.ps1
@@ -1,12 +1,23 @@
-﻿$dbName = $OctopusParameters['DbName']
+﻿function Get-ScriptDirectory
+{
+  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+  Split-Path $Invocation.MyCommand.Path
+}
+
+$dbName = $OctopusParameters['DbName']
 $user = $OctopusParameters['DbUser']
 $pwd = $OctopusParameters['DbPassword']
 $server = $OctopusParameters['DbServerName']
+$currentDirectory = Get-ScriptDirectory
+
+$databasePackagePath = $currentDirectory + '\TaskManager.DatabasePackage.exe'
 
 Write-Host DbName: $dbName
 Write-Host User: $user
 Write-Host Server: $server
+Write-Host $databasePackagePath
 
-& 'TaskManager.DatabasePackage.exe'
+& $databasePackagePath
 
 & sqlcmd -S $server -U $user -P $pwd -i "scripts.sql"
+
