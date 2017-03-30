5d000b0df80f4e98c80f20e0d623426b511c5387
Jon Coello <joncoello@gmail.com>
2017-03-30 10:21:17 +0100

    Can create sql client

---
+++ TaskManager.SqlRepositoriesTests/SQLClientTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.SqlRepositories;
+using Xunit;
+
+namespace TaskManager.SqlRepositoriesTests
+{
+    
+    public class SQLClientTests
+    {
+
+        [Fact]
+        public void SQLClient_Create() {
+            var sut = new SQLClient();
+        }
+
+    }
+}
