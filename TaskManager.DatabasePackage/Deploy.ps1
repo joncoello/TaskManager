45e3db873ddd4b4ff1655bc8ea8f3b8a04d7d246
Jon Coello <joncoello@gmail.com>
2017-03-30 06:03:16 +0100

    add database package using create script approach

---
+++ TaskManager.DatabasePackage/Deploy.ps1
@@ -0,0 +1,12 @@
+﻿$dbName = $OctopusParameters['DbName']
+$user = $OctopusParameters['DbUser']
+$pwd = $OctopusParameters['DbPassword']
+$server = $OctopusParameters['DbServerName']
+
+Write-Host DbName: $dbName
+Write-Host User: $user
+Write-Host Server: $server
+
+& 'TaskManager.DatabasePackage.exe'
+
+& sqlcmd -S $server -U $user -P $pwd -i "scripts.sql"
---
+++ TaskManager.DatabasePackage/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.CreateScripts;
+
+namespace TaskManager.DatabasePackage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Generating script...");
+
+            try
+            {
+                var path = AppDomain.CurrentDomain.BaseDirectory;
+                var outputFilename = "scripts.sql";
+                var createZip = false;
+
+                var scriptCombiner = new CombineScripts(path, outputFilename, createZip);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Script created");
+                
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Script failed");
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
