beb87e34b5efa0ecd4d357fa6eb59296c1999692
Jon Coello <joncoello@gmail.com>
2017-03-31 07:46:21 +0100

    create get single to sql client

--- TaskManager.SqlRepositories/SQLClient.cs
+++ TaskManager.SqlRepositories/SQLClient.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +10,20 @@ namespace TaskManager.SqlRepositories
 {
     public class SQLClient
     {
-        private string connectionString;
+        private string _connectionString;
 
         public SQLClient(string connectionString)
         {
-            this.connectionString = connectionString;
+            this._connectionString = connectionString;
         }
 
-        public object GetSingle<T>(string storedProcedureName, object parameters)
+        public async Task<T> GetSingle<T>(string storedProcedureName, object parameters)
         {
-            throw new NotImplementedException();
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                await conn.OpenAsync();
+                return await conn.QuerySingleAsync<T>(storedProcedureName, commandType: System.Data.CommandType.StoredProcedure, param: parameters);
+            }
         }
     }
 }
--- TaskManager.SqlRepositoriesTests/SQLClientTests.cs
+++ TaskManager.SqlRepositoriesTests/SQLClientTests.cs
@@ -1,44 +1,45 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Configuration;
-//using System.Data.Common;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using TaskManager.SqlRepositories;
-//using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.SqlRepositories;
+using Xunit;
 
-//namespace TaskManager.SqlRepositoriesTests
-//{
-    
-//    public class SQLClientTests
-//    {
+namespace TaskManager.SqlRepositoriesTests
+{
 
-//        [Fact]
-//        public void SQLClient_Create() {
-//            var sut = new SQLClient(null);
-//        }
+    public class SQLClientTests
+    {
 
-//        [Fact]
-//        [UseDatabase]
-//        public void SQLClient_GetSingleEntity()
-//        {
-//            var connectionString = ConfigurationManager.ConnectionStrings["TaskManager"].ConnectionString;
-//            var csb = new DbConnectionStringBuilder();
-//            csb.ConnectionString = connectionString;
-//            csb["database"] = "SQLClientTest";
+        [Fact]
+        public void SQLClient_Create()
+        {
+            var sut = new SQLClient(null);
+        }
 
-//            var sut = new SQLClient(csb.ConnectionString);
+        [Fact]
+        public async Task SQLClient_GetSingleEntity()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["TaskManager"].ConnectionString;
+            var csb = new DbConnectionStringBuilder();
+            csb.ConnectionString = connectionString;
+            csb["database"] = "SQLClientTest";
 
-//            var entity = sut.GetSingle<TestSimpleEntity>("spGetTestSimpleEntity", new {
-//                TestSimpleEntityID = 1
-//            });
+            var sut = new SQLClient(csb.ConnectionString);
 
-//            Assert.NotNull(entity);
-//            Assert.Equal(1, entity.TestSimpleEntityID);
-//            Assert.Equal("bob", entity.TestSimpleEntityName);
+            var entity = await sut.GetSingle<TestSimpleEntity>("spGetTestSimpleEntity", new
+            {
+                TestSimpleEntityID = 1
+            });
 
-//        }
+            Assert.NotNull(entity);
+            Assert.Equal(1, entity.TestSimpleEntityID);
+            Assert.Equal("bob", entity.TestSimpleEntityName);
 
-//    }
-//}
+        }
+
+    }
+}
