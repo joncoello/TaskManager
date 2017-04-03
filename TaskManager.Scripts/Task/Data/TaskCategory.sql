e83e18a7edfefe7ec624ca57eb29b4a31fd38585
Jon Coello <joncoello@gmail.com>
2017-04-03 18:16:41 +0100

    add data script for categories

---
+++ TaskManager.Scripts/Task/Data/TaskCategory.sql
@@ -0,0 +1,16 @@
+﻿if not exists (select * from Task.TaskCategory where CategoryName = 'Urgent')
+	insert into Task.TaskCategory(TaskCategoryID, CategoryName)
+	values(newid(), 'Urgent')
+
+if not exists (select * from Task.TaskCategory where CategoryName = 'Urgent')
+	insert into Task.TaskCategory(TaskCategoryID, CategoryName)
+	values(newid(), 'High')
+
+if not exists (select * from Task.TaskCategory where CategoryName = 'Urgent')
+	insert into Task.TaskCategory(TaskCategoryID, CategoryName)
+	values(newid(), 'Medium')
+
+if not exists (select * from Task.TaskCategory where CategoryName = 'Urgent')
+	insert into Task.TaskCategory(TaskCategoryID, CategoryName)
+	values(newid(), 'Low')
+
