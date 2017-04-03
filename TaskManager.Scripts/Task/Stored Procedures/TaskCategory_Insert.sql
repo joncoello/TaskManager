0a9bc79b72a8056453d870641eda800b6a3bc90b
Jon Coello <joncoello@gmail.com>
2017-04-03 16:24:14 +0100

    chabge ids to guids

---
+++ TaskManager.Scripts/Task/Stored Procedures/TaskCategory_Insert.sql
@@ -0,0 +1,15 @@
+﻿if exists(select * from sys.objects where name = 'TaskCategory_Insert')
+	drop procedure Task.TaskCategory_Insert
+go
+
+Create Procedure Task.TaskCategory_Insert
+
+	@TaskCategoryID uniqueidentifier,
+	@CategoryName varchar(100)
+
+as
+
+	insert into Task.TaskCategory(TaskCategoryID, CategoryName)
+	values(@TaskCategoryID, @CategoryName)
+
+go
--- TaskManager.Scripts/Task/Tables/TaskCategory.sql
+++ TaskManager.Scripts/Task/Tables/TaskCategory.sql
@@ -1,7 +1,8 @@
 ﻿if not exists (select * from sys.tables where name = 'TaskCategory')
 	CREATE TABLE [Task].[TaskCategory] (
-		[TaskCategoryID] INT           IDENTITY (1, 1) NOT NULL,
-		[CategoryName]   VARCHAR (100) NOT NULL,
+		[TaskCategoryID] uniqueidentifier           IDENTITY (1, 1) NOT NULL,
+		[CategoryName]   VARCHAR (100)				NOT NULL,
+
 		CONSTRAINT [PK_TaskCategory] PRIMARY KEY CLUSTERED ([TaskCategoryID] ASC)
 	);
 
