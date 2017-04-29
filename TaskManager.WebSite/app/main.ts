9b9ba12dd889f26bc7366b38e037edf5ef064f80
Jon Coello <joncoello@gmail.com>
2017-04-29 20:38:50 +0100

    fix build issues when cloning repo

--- TaskManager.WebSite/app/main.ts
+++ TaskManager.WebSite/app/main.ts
@@ -1,9 +1,4 @@
-﻿// entry point to aapplication - only bootstrap module
-
-/// <reference path="../typings/globals/core-js/index.d.ts" />
-// above fixes typing issue identified here http://stackoverflow.com/questions/33332394/angular-2-typescript-cant-find-names
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
