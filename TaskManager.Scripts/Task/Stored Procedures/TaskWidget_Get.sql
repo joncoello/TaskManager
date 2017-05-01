8949a29de4ac5f867c7f4a83cdb396d0f64ca8df
Jon Coello <joncoello@gmail.com>
2017-05-01 23:02:01 +0100

    add widget proc

---
+++ TaskManager.Scripts/Task/Stored Procedures/TaskWidget_Get.sql
@@ -0,0 +1,20 @@
+﻿if exists(select * from sys.objects where name = 'TaskWidget_Get')
+	drop procedure Task.TaskWidget_Get
+go
+
+Create Procedure Task.TaskWidget_Get
+
+as
+
+	select
+		tc.CategoryName,
+		Count(ti.TaskItemID) as [count]
+	from
+		Task.TaskItem ti
+		inner join Task.TaskCategory tc on tc.TaskCategoryID = ti.TaskCategoryID
+	group by
+		tc.CategoryName
+	order by
+		tc.CategoryName
+
+go
