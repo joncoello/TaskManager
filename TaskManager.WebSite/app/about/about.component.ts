b5e41d1b4f0a63c46d2f39daec17de20a1e7beb0
Jon Coello <joncoello@gmail.com>
2017-05-19 00:21:44 +0100

    injecting html example

--- TaskManager.WebSite/app/about/about.component.ts
+++ TaskManager.WebSite/app/about/about.component.ts
@@ -12,14 +12,33 @@
                     this is a test
                 </wrapper-example>
                 <button kendoButton (click)="onButtonClick()" [primary]="true">My Kendo UI Button</button>
+                <div id='my-wrapper'>
+                </div>
                 `
 })
 export class AboutComponent {
     public pageTitle: string = 'Examples';
     public loading: boolean = true;
 
+    public theHtmlString: string = `
+                <p class='my-style' onclick='paraClick()'>Hello</p>
+                <style>
+                    .my-style{
+                        color: red;
+                    }
+                </style>
+                <script>
+                    function paraClick(){
+                        alert('boo');
+                    }
+                </script>
+    `;
+
     public onClick(): void {
         this.loading = !this.loading;
+        var el = $('#my-wrapper');
+        el.html(el.html() + this.theHtmlString);
+        el.html(el.html() + this.theHtmlString);
     }
 
     onButtonClick() {
