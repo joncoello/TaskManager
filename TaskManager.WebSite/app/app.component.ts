2140e098c7be70751d48b6d2ebcc9ae5d8ebf145
Jon Coello <joncoello@gmail.com>
2017-05-18 12:47:33 +0100

    close and open sidebar when window size changes

--- TaskManager.WebSite/app/app.component.ts
+++ TaskManager.WebSite/app/app.component.ts
@@ -8,18 +8,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     public pageTitle: string = 'Task Manager';
-
+    
     // collapse nav bar on navigate
     public beforeNav() {
         console.log('before nav');
         var navBar: any = $('.navbar-collapse');
         navBar.collapse('hide');
-        this.openSidebar();
+        this.toggleSidebar();
     }
 
     // open sidebar
-    public openSidebar() {
+    public toggleSidebar() {
         $('#wrapper').toggleClass('toggled');
+    }
+
+    public openSidebar() {
+        $('#wrapper').addClass('toggled');
+    }
+
+    public closeSidebar() {
+        $('#wrapper').removeClass('toggled');
+    }
+
+    public onResize() {
+        this.updateSizing();
+    }
+
+    private isBreakpoint(alias: string): boolean {
+        return $('.device-' + alias).is(':visible');
+    }
+
+    private updateSizing() { // isBreakpoint: (size: string) => boolean
+
+        if (this.isBreakpoint('xs')) {
+
+        }
+        if (this.isBreakpoint('sm')) {
+
+        }
+
+        if (this.isBreakpoint('md')) {
+            this.closeSidebar();
+        }
+
+        if (this.isBreakpoint('lg')) {
+            this.openSidebar();
+        }
+
     }
 
 }
